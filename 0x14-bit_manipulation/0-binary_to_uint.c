e62f6bdd1c3811210c4a5614e95ed78c2647062a
thandolwethu26 <thandolwethu.xulut@gmail.com>
2023-10-26 23:37:31 +0100

    bit manipulations

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,12 +9,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-        int alx;
-        unsigned int abraham = 0;
+        int i;
+        unsigned int dev_val = 0;
 
         if (!b)
                 return (0);
-        for (alx = 0; b[alx]; alx++)
+        for (j = 0; b[j]; j++)
         {
-                if (b[alx] < '0' || b[alx] > '1')
+                if (b[j] < '0' || b[j] > '1')
                         return (0);
+		dec_val = 2 * dec_val + (b[j] - '0');
+        }
+        return (dec_val);
+}
